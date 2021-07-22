30b662116028608e34b2eb5eb7e1792e6c80eb00
ptdk1020 <66972470+ptdk1020@users.noreply.github.com>
2021-07-21 23:49:00 -0400

    more features

--- scripts/feature_script.py
+++ scripts/feature_script.py
@@ -3,7 +3,6 @@
 import feature_maps
 import pandas as pd
 from inspect import getmembers, isfunction
-import os
 
 # a list of pairs (function_name, function) from feature_maps
 functions = getmembers(feature_maps, isfunction)
@@ -13,13 +12,14 @@ def add_features(df, filename):
     for pair in functions:
         if pair[0] not in df.columns:
             df[pair[0]] = df.apply(lambda row: pair[1](row.password), axis=1)
-    df.to_csv(filename)
-    print('Done!')
+        print(pair[0] + ' done!')
+    df.to_csv(filename, index=False)
+    print('Finished!')
     return
 
 
-frame = pd.read_csv('../data/kaggle_processed.csv')
-file = '../data/kaggle_processed.csv'
+frame = pd.read_csv('../data/rockyou_processed.csv')
+file = '../data/rockyou_processed.csv'
 
 add_features(frame, file)
 
