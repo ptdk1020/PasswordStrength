34f4b6b0ea0e038cb629c11ba15a6f26eadf6e1f
ptdk1020 <66972470+ptdk1020@users.noreply.github.com>
2021-07-24 10:43:04 -0400

    modified zxcvbn_features

--- scripts/zxcvbn_features.py
+++ scripts/zxcvbn_features.py
@@ -3,44 +3,47 @@
 from zxcvbn import zxcvbn
 import pandas as pd
 
-test = pd.DataFrame(['password'], columns=['password'])
+#test = pd.DataFrame(['password'], columns=['password'])
 
 
 def zxcvbn_extract(file, destination):
     df = pd.read_csv(file)
-    #df = df['password']
-    #df = test
+    df = pd.DataFrame(df.password)
+    df.dropna(inplace=True)
 
-    cols = ['pw', 'english_wikipedia', 'female_names', 'male_names',
-            'passwords', 'surnames', 'us_tv_and_film']
-    dz = pd.DataFrame(columns=cols)
+    #df = test
+    #df=df.iloc[1000000:2000000, :]
+
+    # create empty dataframe dz
+    cols = ['pw', 'words']
+    dz = pd.DataFrame(columns=cols, index=df.index)
     for col in dz.columns:
         if col == 'pw':
             dz[col] = df['password']
         else:
             dz[col] = ''
 
-
-    for index, row in dz.iterrows():
-        D = zxcvbn(df.iloc[index, 0])
-
+    # use zxcvbn, if a word in a dictionary is detected: <word>,dictionary,bool(l33t),bool(reversed)
+    for id, row in dz.iterrows():
+        D = zxcvbn(df.loc[id, 'password'])
         for item in D['sequence']:
-            if item['pattern'] == 'dictionary':
-                try:
-                    if row[item['dictionary_name']] == '':
-                        row[item['dictionary_name']] = item['matched_word']
-                    else:
-                        row[item['dictionary_name']] = row[item['dictionary_name']] + ',' + item['matched_word']
-                except:
-                    pass
+            try:
+                if row['words'] == '':
+                    row['words'] = item['matched_word'] + ',' + item['dictionary_name'] + ',' + \
+                                   str(item['l33t']) + ',' + str(item['reversed'])
+                else:
+                    row['words'] += ';' + item['matched_word'] + ',' + item['dictionary_name'] \
+                                    + ',' + str(item['l33t']) + ',' + str(item['reversed'])
+            except:
+                pass
 
     dz.to_csv(destination, index=False)
     print('Done')
     return
 
 
-file = '../data/kaggle_processed.csv'
-destination = '../data/kaggle_zxcvbn.csv'
+file = '../data/dataset/BTC1.csv'
+destination = '../data/dataset_zxcvbn/BTC1_zxcvbn.csv'
 
 zxcvbn_extract(file, destination)
 
